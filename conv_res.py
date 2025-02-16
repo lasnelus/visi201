9c06e5d25443afa41333f8b4868c96ed9e75c7cb
Lasnelus <malabretienne@gmail.com*>
2025-02-16 23:41:25 +0100

    commencement du travaiol pour lundi

--- conv_res.py
+++ conv_res.py
@@ -1,8 +1,9 @@
 import subprocess
 
 def execSAT13 (file: str) -> str:
-    """execute le """
+    """execute le sat13"""
     res = subprocess.run("./sat13.exe <" + file, shell=True, capture_output=True, text=True)
+    print(res)
     return str(res.stdout)
     
 
@@ -13,9 +14,9 @@ def miseEnForme (file:str) -> None:
     for i in range(len(output)):
         if output[i] in ["1","2","3","4","5","6"] and output[i-1]==" " and output[i+2] != " ":
             resultat[int(output[i])-1] = output[i+2]
-
+    print(resultat)
     for i in range(len(resultat)):
-        res += resultat[i]
+        res += str(resultat[i])
     sol = open("resSAT13.txt", "w")
     sol.write(str(res))
     sol.close()
