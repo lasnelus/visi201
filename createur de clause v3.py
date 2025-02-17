ba4de921112e1b895fc1b82d3e37dafd2155bcc4
lasnelus <malabretienne@gmail.com>
2025-02-17 10:57:02 +0100

    ajout d'un fonction pour que les piece ne se chevauche pas

--- createur de clause v3.py
+++ createur de clause v3.py
@@ -96,13 +96,30 @@ def creation_clause_tab (piece:list, tab:list)->str:
         res += creation_clause_origine([case[0], case[1]], piece, tab)
     return res
 
-def creation_clause_complet (tab:list)->str:
+
+
+def creation_contrainte_unicite(tab: list, piece: list) -> str:
     """
-    créé toutes les clauses pour que chaque case soit utilisé
+    Crée les clauses interdisant qu'une case soit occupée par plus d'une pièce.
     """
-    res=""
-    for case in tab:
-        res += "C_"+str(case[0])+"_"+str(case[1])+"\n"
+    res = ""
+    case_occupees = {} #cictionnaire ou toute les piece pouvant recouvrir une case sont stocké
+
+    for origine in tab:
+        versions_valides = verif_version(origine, piece, tab)
+        for i, version in enumerate(versions_valides):
+            for case in version:
+                key = (case[0], case[1])
+                if key not in case_occupees:
+                    case_occupees[key] = []
+                case_occupees[key].append(f"P{i}_{origine[0]}_{origine[1]}")
+
+
+    for case, pieces in case_occupees.items():
+        for i in range(len(pieces)):
+            for j in range(i + 1, len(pieces)):
+                res += f"~{pieces[i]} ~{pieces[j]}\n"  # Pas deux pièces sur la même case
+
     return res
 
 def ecriture_clause (clause: str)-> None:
@@ -113,5 +130,7 @@ def ecriture_clause (clause: str)-> None:
     fichier.write(clause)
     fichier.close()
 
-
-ecriture_clause(str(creation_clause_tab(piece, lecteur_tab("tab.txt")))+str(creation_clause_complet(lecteur_tab("tab.txt"))))
+ecriture_clause(
+    str(creation_clause_tab(piece, lecteur_tab("tab.txt"))) +
+    str(creation_contrainte_unicite(lecteur_tab("tab.txt"), piece))
+)
